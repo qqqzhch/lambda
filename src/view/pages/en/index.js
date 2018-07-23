d85f7de420e4db278a3dbbb57855b17b9c11e9cf
zhangchao <544262408@qq.com>
2018-07-23 16:44:34 +0800

    分享图片

--- src/view/pages/en/index.js
+++ src/view/pages/en/index.js
@@ -2,9 +2,9 @@
 const content = require('./index.ejs');
 const layout = require('../../layout/html.js');
 const pageConfig = {
-	pageTitle: 'Lambda - 去中心化结构云存储',
-	description: 'Lambda - 去中心化结构云存储',
-	keywords: 'Lambda,去中心化,云存储,去中心化结构云存储',
+	pageTitle: 'Lambda -  A Disruptor in Blockchain-Based Storage Solution',
+	description: 'Lambda - Providing Unlimited Storage Capabilities',
+	keywords: 'Lambda,Blockchain,Storage',
 	pageId: 'indexEn'
 };
 
