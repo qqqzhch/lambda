84a40e03fe625cc65edc3199ea896e9692a58906
zhangchao <544262408@qq.com>
2018-07-26 16:31:26 +0800

    合作logo 简介

--- src/view/pages/en/index.js
+++ src/view/pages/en/index.js
@@ -3,7 +3,7 @@ const content = require('./index.ejs');
 const layout = require('../../layout/html.js');
 const pageConfig = {
 	pageTitle: 'Lambda -  A Disruptor in Blockchain-Based Storage Solution',
-	description: 'Lambda - Providing Unlimited Storage Capabilities',
+	description: 'Lambda is a fast, safe, and scalable blockchain infrastructure project, which provides decentralized applications (DAPPs) data storage capabilities with unlimited scalability and fulfills services such as multi-chain data co-storage, cross-chain data management, data privacy protection, Proof of Recoverability of data (POR), Provable Data Possession (PDP), and distributed intelligent computing through logic decoupling and independent implementation of Lambda Chain and Lambda DB.',
 	keywords: 'Lambda,Blockchain,Storage',
 	pageId: 'indexEn'
 };
