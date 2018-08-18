5ea981af10e48653cd66548fad6681ea0b284a19
zhangchao <544262408@qq.com>
2018-08-18 17:08:11 +0800

    seo setup

--- src/view/pages/en/index.js
+++ src/view/pages/en/index.js
@@ -2,9 +2,9 @@
 const content = require('./index.ejs');
 const layout = require('../../layout/html.js');
 const pageConfig = {
-	pageTitle: 'Lambda -  A Disruptor in Blockchain-Based Storage Solution',
-	description: 'Lambda is a fast, safe, and scalable blockchain infrastructure project, which provides decentralized applications (DAPPs) data storage capabilities with unlimited scalability and fulfills services such as multi-chain data co-storage, cross-chain data management, data privacy protection, Proof of Recoverability of data (POR), Provable Data Possession (PDP), and distributed intelligent computing through logic decoupling and independent implementation of Lambda Chain and Lambda DB.',
-	keywords: 'Lambda,Blockchain,Storage',
+	pageTitle: 'Lambda blockchain  -  A Disruptor in Blockchain-Based Storage Solution',
+	description: 'Lambda blockchain  is a fast, safe, and scalable blockchain infrastructure project',
+	keywords: 'Lambda,Blockchain,Storage,Dapps,IPFS,filecoin,aws',
 	pageId: 'indexEn'
 };
 
