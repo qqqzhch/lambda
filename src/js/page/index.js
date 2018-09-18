bfcfb8c016f998111f4fb4e700595feded7af20e
kerinlin <18296613547@qq.com>
2018-09-18 21:27:45 +0800

    活动版面slider

--- src/js/page/index.js
+++ src/js/page/index.js
@@ -7,77 +7,90 @@ require("../../css/common/grid.less");
 require("../../css/common/header.less");
 require("../../css/common/footer.less");
 require("../../css/page/index.less");
-require("../../../node_modules/swiper/dist/css/swiper.min.css")
+require("../../../node_modules/swiper/dist/css/swiper.min.css");
 //引入 js
+require("../components/jquery-confirm/jquery-confirm.js");
+require("../components/jquery-confirm/jquery-confirm.less");
 require("../../js/components/header.js");
 require("../../js/components/cookie.js");
-require('jquery-countdown');
-require('../../../node_modules/swiper/dist/js/swiper.min.js')
-var Swiper = require('swiper');
+require("jquery-countdown");
+require("../../../node_modules/swiper/dist/js/swiper.min.js");
+var Swiper = require("swiper");
 
-var queryString=require('query-string');
+var queryString = require("query-string");
 // import queryString from 'query-string'
 
-$(document).ready(function(){
-	// 增加事件
-	//倒计时
-	// $('#getting-started').countdown('2018/06/30', function(event) {
-	// 	$(this).html('<p>'+event.strftime('%n')+'</p>:'+'<p>'+event.strftime('%H')+'</p>:'+'<p>'+event.strftime('%M')+'</p>:'+'<p>'+event.strftime('%S')+'</p>');
-	// });
+$(document).ready(function() {
+  // 增加事件
+  //倒计时
+  $("#time").countdown(new Date(2018, 8,25, 14, 0, 0), function(event) {
+    $(this).html(
+      "<p>" +
+        event.strftime("%n") +
+        "</p>:" +
+        "<p>" +
+        event.strftime("%H") +
+        "</p>:" +
+        "<p>" +
+        event.strftime("%M") +
+        "</p>:" +
+        "<p>" +
+        event.strftime("%S") +
+        "</p>"
+    );
+  });
 
+  const parsed = queryString.parse(location.search);
+  var from = parsed.from;
+  //#tokenregister
+  //#tokenwhitelist
 
-	var starttime = new Date(2018,8,19,14,0,0);
-  setInterval(function () {
-    var nowtime = new Date();
-    var time = starttime - nowtime;
-    var day = parseInt(time / 1000 / 60 / 60 / 24);
-    var hour = parseInt(time / 1000 / 60 / 60 % 24);
-    var minute = parseInt(time / 1000 / 60 % 60);
-    var seconds = parseInt(time / 1000 % 60);
-		$('.day').html(day+"D");
-		$('.hour').html(hour+"H");
-		$('.minute').html(minute+"M");
-		$('.seconds').html(seconds+"S");
-  }, 1000);
+  if (from) {
+    $("#tokenregister").attr(
+      "href",
+      "https://token.lambda.im/index.html?from=" + from + "#/register"
+    );
+    $("#tokenwhitelist").attr(
+      "href",
+      "https://token.lambda.im/index.html?from=" + from + "#/register"
+    );
+  }
 
+  $(".btn").click(function() {
+		$.alert('ICO is coming soon！')
+    // console.log('asdasd')
+  });
+  $(".line").css("width", window.screen.width);
 
-	const parsed = queryString.parse(location.search);
-	var from=parsed.from;
-	//#tokenregister
-	//#tokenwhitelist
-	 
-	if(from){
-		$('#tokenregister').attr('href','https://token.lambda.im/index.html?from='+from+'#/register')
-		$('#tokenwhitelist').attr('href','https://token.lambda.im/index.html?from='+from+'#/register')
+  $(".principle .item").on("click", function() {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    var _index = $(this).index() - 1;
+    $(".principle .tab-contents div")
+      .eq(_index)
+      .addClass("contents-active")
+      .siblings()
+      .removeClass("contents-active");
+  });
 
-	}
-
-
-	$('.line').css('width',window.screen.width);
-
-	$('.principle .item').on('click',function(){
-		$(this).addClass('active').siblings().removeClass('active');
-		var _index = $(this).index()-1;
-		$('.principle .tab-contents div').eq(_index).addClass('contents-active').siblings().removeClass('contents-active');
-	})
-	
-	var mySwiper = new Swiper ('.swiper-container', {
-		pagination: {
-			el: '.swiper-pagination',
-			clickable :true,
-			type: 'bullets',
-			bulletClass : 'my-bullet',
-			bulletActiveClass: 'my-bullet-active',
-		},
-		loop: true,
-		// autoplay:{
-		// 	disableOnInteraction: true,
-		// },
-		// speed: 4000,
-		// navigation: {
+  var mySwiper = new Swiper(".swiper-container", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: "bullets",
+      bulletClass: "my-bullet",
+      bulletActiveClass: "my-bullet-active"
+    },
+    loop: true
+    // autoplay:{
+    // 	disableOnInteraction: true,
+    // },
+    // speed: 4000,
+    // navigation: {
     //   nextEl: '.swiper-button-next',
     //   prevEl: '.swiper-button-prev',
     // },
-	})
-
+  });
 });
