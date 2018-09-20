e678e386e84551cd5de04b656c8e3997c7194cad
kerinlin <18296613547@qq.com>
2018-09-20 16:28:11 +0800

    新增切换语言功能

--- src/js/page/index.js
+++ src/js/page/index.js
@@ -104,4 +104,13 @@ $(document).ready(function() {
     $(".in").css("display","block");
     $(".out").css("display","none");
   }
+
+  $('.ru').click(function(){
+		$('.img').attr('src',"https://cdn.wpml.org/wp-content/plugins/sitepress-multilingual-cms/res/flags/ru.png?x53954");
+		$('.title').html('Russia');
+	})
+	$('.en').click(function(){
+		$('.img').attr('src',"https://www.coinex8.com/static/media/02_02.language_english.67e1a74c.svg");
+		$('.title').html('English');
+	})
 });
